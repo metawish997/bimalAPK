1438a1026c466ccf4ee57d48360e316efc592bb4
unknown <bhavishybhaisaniya1432@gmail.com>
2026-08-12 12:29:25 +0530

    fix: update split apk plugin for newer expo versions

--- plugins/withSplitApk.js
+++ plugins/withSplitApk.js
@@ -3,9 +3,19 @@ const { withAppBuildGradle } = require('@expo/config-plugins');
 module.exports = function withSplitApk(config) {
   return withAppBuildGradle(config, (config) => {
     if (config.modResults.language === 'groovy') {
+      const splitsBlock = `
+    splits {
+        abi {
+            reset()
+            enable true
+            universalApk false
+            include "armeabi-v7a", "x86", "arm64-v8a", "x86_64"
+        }
+    }
+`;
       config.modResults.contents = config.modResults.contents.replace(
-        /def enableSeparateBuildPerCPUArchitecture = false/,
-        'def enableSeparateBuildPerCPUArchitecture = true'
+        /android\s*\{/,
+        `android {${splitsBlock}`
       );
     }
     return config;
